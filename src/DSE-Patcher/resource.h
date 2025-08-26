2faa25d0cf44f239d3b6c81270a25ecdc1fb7d26
Dmitry Warkentin <dmitry@warkentin.ru>
2025-08-27 03:53:39 +0400

    Add files via upload

---
+++ src/DSE-Patcher/resource.h
@@ -0,0 +1,14 @@
+// Dialogs
+#define IDD_DIALOG1                     101
+
+// Icons
+#define IDI_ICON1                       102
+#define IDI_ICON2                       103
+
+// Controls
+#define IDC_COMBO1                      1001
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON3                     1004
+#define IDC_STATIC1                     1005
+#define IDC_STATUS_BAR1                 1006
